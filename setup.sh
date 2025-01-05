c069fef6d3129ca47cf3bd3db1ea9a8e204670c3
Arry-eng <120550216+Arry-eng@users.noreply.github.com>
2025-01-05 17:12:20 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # Skipping update as it takes a lot of compute time and energy
 # Package installation os sl was not found if update is skipped.
 # So reanabling update and rebuilding the container image aftr commit and push
-sudo apt-get update
+# sudo apt-get update
 sudo apt-get install sl
-echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
+sudo apt search 'sl'
+# echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
