6bb98cb590fd9548a2eb1c40d0b7d69dc4985e87
Arry-eng <120550216+Arry-eng@users.noreply.github.com>
2025-01-05 07:19:43 +0000

    # So reanabling update and rebuilding the container image aftr commit and push

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Skipping update as it takes a lot of compute time and energy
-# sudo apt-get update
+# Package installation os sl was not found if update is skipped.
+# So reanabling update and rebuilding the container image aftr commit and push
+sudo apt-get update
 sudo apt-get install sl
 echo "export PATH=\$PATH:/usr/games" >> ~/.bashrc
